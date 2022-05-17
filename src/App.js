f093a24a72083c8fd36e7a4a4a75fca39e768fa2
Will Gundy <willgunderson503@gmail.com>
2022-05-17 16:41:22 -0700

    added fetch utils file

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <PokemonSearch />
       <YelpSearch />
-    { /* stretch goal: <WeatherSearch /> */ }
+      <WeatherSearch /> 
     </div>
   );
 }
