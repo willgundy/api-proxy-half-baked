3a1ce9c350627cb11d36235d58b564e51f4a4401
Will Gundy <willgunderson503@gmail.com>
2022-05-18 11:43:34 -0700

    split out pages and added a header

--- src/App.js
+++ src/App.js
@@ -2,14 +2,32 @@ import PokemonSearch from './PokemonSearch';
 import YelpSearch from './YelpSearch';
 import WeatherSearch from './WeatherSearch';
 import './App.css';
+import Header from './Header';
+import { Switch, Route, Redirect, BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <PokemonSearch />
-      <YelpSearch />
-      <WeatherSearch /> 
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <main>
+          <Switch>
+            <Route exact path="/">
+              <Redirect to={'/pokemon'}/>
+            </Route>
+            <Route exact path="/pokemon" >
+              <PokemonSearch />
+            </Route>
+            <Route exact path="/yelp" >
+              <YelpSearch />
+            </Route>
+            <Route exact path="/weather" >
+              <WeatherSearch /> 
+            </Route>
+          </Switch>
+        </main>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+export default function Header() {
+  return (
+    <header>
+      <NavLink className='Nav' exact to={'/pokemon'}>Pokedex</NavLink>
+      <NavLink className='Nav' exact to={'/weather'}>Weather</NavLink>
+      <NavLink className='Nav' exact to={'/yelp'}>Yelp</NavLink>
+    </header>
+  );
+}
